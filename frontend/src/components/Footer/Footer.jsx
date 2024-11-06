1d844c901683725d5edb4f473df6072704b2b06e
Krish Kapadia <kkapadia0202@gmail.com>
2024-11-06 22:34:42 +0530

    First Commit

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+            <img src={assets.logo} alt="" />
+            <p>With Crave, you can explore a variety of cuisines, discover new favorites, and even customize your meals to suit your taste. Our dedicated delivery team ensures that your food arrives fresh and on time, making your dining experience as seamless as possible. Whether you are planning a cozy night in or a lively gathering, Crave brings delicious food right to your doorstep, satisfying your cravings anytime, anywhere!</p>
+            <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+            </div>
+        </div>
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>+1-212-456-7890</li>
+                <li>contact@tomato.com</li>
+            </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2024 © Tomato.com - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
